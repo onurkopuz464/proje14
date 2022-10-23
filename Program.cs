95e54c7d377c5d4fa20197bc66f30f2f6e061512
onurkopuz464 <onurkopuz464@gmail.com>
2022-10-23 22:27:19 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        function instance= new function();
+        
+        
+        //Console.WriteLine(instance.functionOne(4,5));
+        int[] array = {1,2,3};
+        array.arraychange();
+        array.arraywrite();
+    
+
+
+    }
+    class function 
+    {
+        public int functionOne()
+        {
+            return 0;
+        }
+
+    
+
+
+    }
+ 
+        
+}
+   public static class Extension
+    {
+        public static int[] arraychange(this int[] param)
+        {
+            Array.Sort(param);
+            return param;
+        }
+        public static void arraywrite(this int[] param)
+        {
+            foreach(var item in param)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+
+
+    }
